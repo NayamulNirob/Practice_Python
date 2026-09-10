552240d4d1f99fbcc70d122c8ef608637569d2b9
Md. Neyamul Islam <158471735+NayamulNirob@users.noreply.github.com>
2026-09-10 19:57:48 +0600

    Create Backslash_character.py

---
+++ Backslash_character.py
@@ -0,0 +1,32 @@
+#Backslash character practice
+print("Hello World\nThis is a new line")
+
+print("Hello World\tThis is a tab space")
+
+print("Hello World\\This is a backslash")
+
+print("Hello World\'This is a single quote\'")
+
+print("Hello World\"This is a double quote\"")
+
+print("Hello World\bThis is a backspace")
+
+print("Hello World\rThis is a carriage return")
+
+print("Hello World\fThis is a form feed")
+
+print("Hello World\vThis is a vertical tab")
+
+print("Hello World\aThis is a bell sound")
+
+print("Hello World\0This is a null character")
+
+print("Hello World\123This is an octal value")
+
+print("Hello World\x7BThis is a hexadecimal value")
+
+print("Hello World\u0041This is a unicode value")
+
+print("Hello World \U00000041 This is a unicode value")
+
+print("Hello World \N{LATIN CAPITAL LETTER B} This is a named unicode value")
