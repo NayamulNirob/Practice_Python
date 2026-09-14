1767d663a91e95285e7d3f73a7363303b1cca222
Md. Neyamul Islam <158471735+NayamulNirob@users.noreply.github.com>
2026-09-14 19:45:08 +0600

    easySwppings

---
+++ Swapping.py
@@ -0,0 +1,13 @@
+a= 20
+b=10
+# temp=0
+# temp=a # temp =20
+# a=b # a =10
+# b=temp # b =20
+# print("The value of a after swapping: ",a)
+# print("The value of b after swapping: ",b)
+
+#Easy way to swap two numbers in python
+a,b=b,a
+print("The value of a after swapping: ",a)
+print("The value of b after swapping: ",b)
