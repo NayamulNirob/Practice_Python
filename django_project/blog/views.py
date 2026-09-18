82ddb6bf57fa148e3f694be10aea483c85c6df76
Md. Neyamul Islam <158471735+NayamulNirob@users.noreply.github.com>
2026-09-18 17:28:16 +0600

    Add sample posts; update views and templates
    Introduce a sample posts list (with datetime) in blog.views and have home() pass posts via context. Update blog/home.html to loop and display post titles. Tweak about.html heading text. Add identifier.sqlite to the repository.

--- django_project/blog/views.py
+++ django_project/blog/views.py
@@ -1,9 +1,30 @@
+import datetime
+
 from django.shortcuts import render
 from django.http import HttpResponse
-# Create your views here.
 
+posts=[
+    {'auther':'John dou',
+     'title' :'John dou\'s blog',
+     'content':'John dou first blog.........',
+     'date_posted':datetime.datetime.now()
+     },
+     {'auther':'Steven byard',
+     'title' :'Steven byard\'s blog',
+     'content':'Steven byard first blog.........',
+     'date_posted':datetime.datetime.now()
+     },
+    {'auther':'Bob hone',
+     'title' :'Bob hone\'s blog',
+     'content':'Bob hone first blog.........',
+     'date_posted':datetime.datetime.now()
+     }
+]
+
+# Create your views here.
 def home(request):
-    return render(request,'blog/home.html')
+    context = {'posts':posts}
+    return render(request,'blog/home.html',context)
 
 def about(request):
     return render(request,'blog/about.html')
