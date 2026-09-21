9a68268edf63d10d7acd0a6fa6e7ed8f6f088173
Md. Neyamul Islam <158471735+NayamulNirob@users.noreply.github.com>
2026-09-21 15:55:01 +0600

    talk with database

--- django_project/blog/views.py
+++ django_project/blog/views.py
@@ -2,28 +2,32 @@ import datetime
 
 from django.shortcuts import render
 from django.http import HttpResponse
+from .models import Post
 
-posts=[
-    {'auther':'John dou',
-     'title' :'John dou\'s blog',
-     'content':'John dou first blog.........',
-     'date_posted':datetime.date.today()
-     },
-     {'auther':'Steven byard',
-     'title' :'Steven byard\'s blog',
-     'content':'Steven byard first blog.........',
-     'date_posted':datetime.date.today()
-     },
-    {'auther':'Bob hone',
-     'title' :'Bob hone\'s blog',
-     'content':'Bob hone first blog.........',
-     'date_posted':datetime.date.today()
-     }
-]
+# posts=[
+#     {'auther':'John dou',
+#      'title' :'John dou\'s blog',
+#      'content':'John dou first blog.........',
+#      'date_posted':datetime.date.today()
+#      },
+#      {'auther':'Steven byard',
+#      'title' :'Steven byard\'s blog',
+#      'content':'Steven byard first blog.........',
+#      'date_posted':datetime.date.today()
+#      },
+#     {'auther':'Bob hone',
+#      'title' :'Bob hone\'s blog',
+#      'content':'Bob hone first blog.........',
+#      'date_posted':datetime.date.today()
+#      }
+# ]
 
 # Create your views here.
 def home(request):
-    context = {'posts':posts}
+    context = {
+        # 'posts': posts
+        'posts': Post.objects.all(),
+    }
     return render(request,'blog/home.html',context)
 
 def about(request):
