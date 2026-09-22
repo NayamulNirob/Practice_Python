ebf146cd0d34e23da319cb2da72b0f5d55d5655a
Md. Neyamul Islam <158471735+NayamulNirob@users.noreply.github.com>
2026-09-22 20:47:17 +0600

    Add Profile model, media support and profile UI
    Introduce a Profile model (with ImageField) and generated migration; register Profile in admin. Enable MEDIA_ROOT and MEDIA_URL in settings and serve media files via urls.py when DEBUG. Add default and sample profile images and update profile template to display user avatar and info. Also add note to install Pillow and include updated sqlite DB from the migration.

--- django_project/django_project/settings.py
+++ django_project/django_project/settings.py
@@ -9,7 +9,7 @@ https://docs.djangoproject.com/en/6.1/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/6.1/ref/settings/
 """
-
+import os.path
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -119,6 +119,9 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/6.1/howto/static-files/
 
 STATIC_URL = 'static/'
+MEDIA_ROOT = BASE_DIR / 'media'
+# MEDIA_ROOT= os.path.join(BASE_DIR,'media')
+MEDIA_URL = '/media/'
 
 CRISPY_ALLOWED_TEMPLATE_PACKS = 'bootstrap5'
 CRISPY_TEMPLATE_PACK = 'bootstrap5'
--- django_project/django_project/urls.py
+++ django_project/django_project/urls.py
@@ -18,6 +18,8 @@ from django.contrib import admin
 from django.urls import path,include
 from users import views as user_views
 from django.contrib.auth import views as auth_views
+from django.conf.urls.static import static
+from django.conf import settings
 
 from blog import views
 
@@ -28,5 +30,8 @@ urlpatterns = [
     path('login/',auth_views.LoginView.as_view(template_name='users/login.html') , name='login'),
     path('logout/',user_views.logout_view , name='logout'),
     path('', include('blog.urls')),
-
 ]
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+
+
