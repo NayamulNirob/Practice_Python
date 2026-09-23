5af3eb7e867adbdcbcf708f04038fe73f18c7fb2
Md. Neyamul Islam <158471735+NayamulNirob@users.noreply.github.com>
2026-09-23 19:11:02 +0600

    Add profile image, update forms/views/templates
    Display and manage user profile images: add ProfileUpdateForm and UserUpdateForm, render profile update form in template (with crispy forms and file upload), and show author profile image on blog home. Add image resize logic to Profile.save using Pillow. Update profile view to handle POST/validation, save changes, and show success message. Includes added media profile images and updated sqlite DB.

--- django_project/users/views.py
+++ django_project/users/views.py
@@ -2,9 +2,11 @@ from django.contrib.auth import logout
 from django.shortcuts import render, redirect
 # from django.contrib.auth.forms import  UserCreationForm
 from django.contrib import messages
-from .forms import UserRegistrationForm
+from .forms import UserRegistrationForm, UserUpdateForm, ProfileUpdateForm
 from django.contrib.auth.decorators import login_required
 from django.conf import settings
+
+
 # Create your views here.
 
 
@@ -20,12 +22,38 @@ def register(request):
     else:
         # form = UserCreationForm() # built in form of django
         form = UserRegistrationForm()
-    return render(request,'users/register.html',{'form':form})
+    return render(request, 'users/register.html', {'form': form})
+
 
 def logout_view(request):
     logout(request)
-    return render(request,'users/logout.html')
+    return render(request, 'users/logout.html')
+
 
 @login_required(login_url='/login/')
 def profile(request):
-    return render(request,'users/profile.html')
+    if request.method == 'POST':
+        u_form = UserUpdateForm(
+            request.POST,
+            instance=request.user
+        )
+        p_form = ProfileUpdateForm(
+            request.POST,
+            request.FILES,
+            instance=request.user.profile
+        )
+        if u_form.is_valid() and p_form.is_valid():
+            u_form.save()
+            p_form.save()
+            messages.success(request, f'Your account has been updated!')
+            return redirect('profile')
+    else:
+        u_form = UserUpdateForm(instance=request.user)
+        p_form = ProfileUpdateForm(instance=request.user.profile)
+
+    context = {
+        'u_form': u_form,
+        'p_form': p_form
+    }
+
+    return render(request, 'users/profile.html', context)
