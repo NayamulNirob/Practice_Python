115f46cb0318c36958531b51a251e290ccf949a9
Md. Neyamul Islam <158471735+NayamulNirob@users.noreply.github.com>
2026-09-23 20:27:24 +0600

    Add post detail page and use class-based views
    Introduce PostListView and PostDetailView (ListView/DetailView) in views.py and switch the home route to PostListView. Add URL pattern for 'post/<int:pk>/' in urls.py. Update home.html to link post titles to the new post-detail URL and add post_detail.html template to render individual posts. Also tidy imports and sample comment typos. This enables per-post detail pages and replaces the function-based home view with a class-based list view.

--- django_project/blog/urls.py
+++ django_project/blog/urls.py
@@ -2,9 +2,12 @@
 from django.contrib import admin
 from django.urls import path
 from . import views
+from.views import PostListView,PostDetailView
 
 
 urlpatterns = [
-    path('', views.home, name='blog-home'),
+    path('', PostListView.as_view(), name='blog-home'),
+
+    path('post/<int:pk>/', PostDetailView.as_view(), name='post-detail'),
     path('about/', views.about, name='blog-about'),
 ]
--- django_project/blog/views.py
+++ django_project/blog/views.py
@@ -3,19 +3,19 @@ import datetime
 from django.shortcuts import render
 from django.http import HttpResponse
 from .models import Post
-
+from django.views.generic import ListView,DetailView,CreateView,UpdateView,DeleteView
 # posts=[
-#     {'auther':'John dou',
+#     {'author':'John dou',
 #      'title' :'John dou\'s blog',
 #      'content':'John dou first blog.........',
 #      'date_posted':datetime.date.today()
 #      },
-#      {'auther':'Steven byard',
+#      {'author':'Steven byard',
 #      'title' :'Steven byard\'s blog',
 #      'content':'Steven byard first blog.........',
 #      'date_posted':datetime.date.today()
 #      },
-#     {'auther':'Bob hone',
+#     {'author':'Bob hone',
 #      'title' :'Bob hone\'s blog',
 #      'content':'Bob hone first blog.........',
 #      'date_posted':datetime.date.today()
@@ -29,6 +29,16 @@ def home(request):
         'posts': Post.objects.all(),
     }
     return render(request,'blog/home.html',context)
+
+
+class PostListView(ListView):
+    model = Post
+    template_name = 'blog/home.html' #<app>/<model>_<viewtype>.html
+    context_object_name = 'posts'
+    ordering = ['-date_posted']
+
+class PostDetailView(DetailView):
+    model = Post
 
 def about(request):
     return render(request,'blog/about.html',{'title':'About'})
